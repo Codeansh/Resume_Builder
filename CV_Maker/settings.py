6e5ab079b9ab2ec6feeddde196bcd930b6387f33
Shivansh <ansh02shiv@gmail.com>
2023-09-10 18:25:34 +0530

    Fixes some issues

--- CV_Maker/settings.py
+++ CV_Maker/settings.py
@@ -123,7 +123,7 @@ STATICFILES_DIRS = (
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
+CSRF_TRUSTED_ORIGINS = ['https://resume-builder-jzef.onrender.com']
 CRISPY_TEMPLATE_PACK = "bootstrap4"
 
 LOGIN_REDIRECT_URL = "/"
